1a868dd3edc16f737b6fbc5175a07e84ddd1afac
reed <reed@albus>
2022-05-18 12:39:38 -0500

    JVM logging now does Log4j logger implementation.

--- scripts/run-graph-writer.sh
+++ scripts/run-graph-writer.sh
@@ -1,16 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 # ****************************************************************************
 # * run graph writing server
 # ****************************************************************************
 
 echo starting GraphWriter server
 pwd
-
-LIB=lib
-CLASSPATH=$LIB/GraphWriter-1.0.jar
-CLASSPATH=$CLASSPATH:$LIB/disruptor-3.3.6.jar
-CLASSPATH=$CLASSPATH:$LIB/junit-4.7.jar
-CLASSPATH=$CLASSPATH:$LIB/log4j-1.2.17.jar
+CLASSPATH=lib/GraphWriter-1.0.jar:$(<classpathFile);
+#echo CLASSPATH: ${CLASSPATH}
 
 java -version
-java -ea  -Djava.net.preferIPv6Addresses=false -Djava.net.preferIPv4Stack=true -classpath $CLASSPATH org.texai.graphwriter.GraphWriter
+java -ea  -Djava.net.preferIPv6Addresses=false -Djava.net.preferIPv4Stack=true -classpath ${CLASSPATH}  org.texai.graphwriter.GraphWriter
